5f4278041d8829180d0ff46a8847f6c3c0de069c
JasonH2o <jasonliu820@hotmail.com>
2017-08-14 00:58:20 -0700

    Modified gitignore file

--- Pccma.Services.PraiseTeam.Tests/UnitTest1.cs
+++ Pccma.Services.PraiseTeam.Tests/UnitTest1.cs
@@ -6,9 +6,10 @@ namespace Pccma.Services.PraiseTeam.Tests
     [TestClass]
     public class UnitTest1
     {
+        public string temp;
         [TestMethod]
         public void TestMethod1()
-        {
+        {            
         }
     }
 }
